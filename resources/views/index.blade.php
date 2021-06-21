3760ce290b2ef1cc6fee531e8f49c421c30ab746
Rizal Muhamad H <rizalmuhamadh@gmail.com>
2021-06-21 17:11:33 +0700

    add line clamp & swiper

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,58 +6,167 @@
 
     <div class="max-w-6xl mx-auto my-8">
         <div class="flex flex-wrap">
-            <div class="lg:w-2/3 w-full">
-                <div class="lg:m-0 m-3 first-featuredbg-cover relative bg-cover overflow-hidden rounded shadow-lg"
-                    style="background-image: url(https://picsum.photos/780/420); height: 420px;">
-                    <div class="transition-3 overlay-500 lg:p-10 p-3 w-full h-full text-white">
+            <div class="swiper-container lg:w-2/3 w-full">
+                <!-- Additional required wrapper -->
+                <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    <div class="swiper-slide">
+                        <div class="relative h-full">
+                            <div class="absolute z-20 bottom-0 left-0 flex flex-col px-3 py-3">
+                                <div class="text-white text-sm py-2">
+                                    31 September 2021
+                                </div>
+                                <p class="font-medium text-white text-2xl line-clamp-2">
+                                    Kabupaten Cirebon Catat Lonjakan Kasus Covid-19 Tertinggi
+                                </p>
+                            </div>
 
-                        <div class="read-full-store absolute p-5 right-0 bottom-0">
-                            <a href="single-post.html"
-                                class="font-light border-b pb-1 border-gray-300 hover:border-gray-100 hover:text-gray-100">Read
-                                Full Story</a>
+                            <div class="h-full w-full bg-gray-600 absolute z-10 rounded-xl border-none opacity-30"></div>
+                            <img class="rounded-xl w-full h-full align-middle border-none"
+                                src="https://www.ayocirebon.com/images-cirebon/post/articles/2021/05/07/10894/masjid_pusaka_baiturrahmah_-_imy.jpg"
+                                alt="" />
                         </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="relative h-full">
+                            <div class="absolute z-20 bottom-0 left-0 flex flex-col px-3 py-3">
+                                <div class="text-white text-sm py-2">
+                                    31 September 2021
+                                </div>
+                                <p class="font-medium text-white text-2xl line-clamp-2">
+                                    Kabupaten Cirebon Catat Lonjakan Kasus Covid-19 Tertinggi
+                                </p>
+                            </div>
 
-                        <div class="info text-sm">
-                            By <span class="font-semibold">Amir Rehman</span> On <span class="font-semibold">24 August,
-                                2019</span>
+                            <div class="h-full w-full bg-gray-600 absolute z-10 rounded-xl border-none opacity-30"></div>
+                            <img class="rounded-xl w-full h-full align-middle border-none"
+                                src="https://www.ayocirebon.com/images-cirebon/post/articles/2021/05/07/10894/masjid_pusaka_baiturrahmah_-_imy.jpg"
+                                alt="" />
                         </div>
-                        <h1 class="leading-snug text-3xl font-bold my-4">Optimize your web desing process with rapid
-                            prototyping</h1>
-                        <p class="text-normal leading-normal font-light">Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Quia consequuntur natus laudantium saepe ducimus cum non at aperiam incidunt provident
-                            minus fuga voluptatibus aspernatur eos quam, aliquam ullam neque distinctio?</p>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="relative h-full">
+                            <div class="absolute z-20 bottom-0 left-0 flex flex-col px-3 py-3">
+                                <div class="text-white text-sm py-2">
+                                    31 September 2021
+                                </div>
+                                <p class="font-medium text-white text-2xl line-clamp-2">
+                                    Kabupaten Cirebon Catat Lonjakan Kasus Covid-19 Tertinggi
+                                </p>
+                            </div>
+
+                            <div class="h-full w-full bg-gray-600 absolute z-10 rounded-xl border-none opacity-30"></div>
+                            <img class="rounded-xl w-full h-full align-middle border-none"
+                                src="https://www.ayocirebon.com/images-cirebon/post/articles/2021/05/07/10894/masjid_pusaka_baiturrahmah_-_imy.jpg"
+                                alt="" />
+                        </div>
                     </div>
                 </div>
+                <!-- If we need navigation buttons -->
+                <div class="swiper-button-prev text-gray-600"></div>
+                <div class="swiper-button-next"></div>
+
             </div>
+            {{-- <div class="relative lg:w-2/3 w-full">
+                <div class="absolute z-20 bottom-0 left-0 flex flex-col px-3 py-3">
+                    <div class="text-white text-sm py-2">
+                        31 September 2021
+                    </div>
+                    <p class="font-medium text-white text-2xl line-clamp-2">
+                        Kabupaten Cirebon Catat Lonjakan Kasus Covid-19 Tertinggi
+                    </p>
+                </div>
+
+                <div class="h-full w-full bg-gray-600 absolute z-10 rounded-xl border-none opacity-30"></div>
+                <img class="rounded-xl w-full h-full align-middle border-none"
+                    src="https://www.ayocirebon.com/images-cirebon/post/articles/2021/05/07/10894/masjid_pusaka_baiturrahmah_-_imy.jpg"
+                    alt="" />
+            </div> --}}
             <div class="lg:w-1/3 w-full">
-                <div class="feature-list p-3 pt-0 lg:px-8">
+                <div class="feature-list p-3 pt-0 lg:px-4">
                     <h2 class="text-2xl font-semibold">Featured</h2>
                     <div class="list">
 
-                        <div class="item my-3 border leading-normal border-gray-300 p-3 rounded">
-                            <div class="info text-sm font-semibold mb-1"><a href="category-page.html"
-                                    class="no-underline uppercase hover:underline text-gray-700 text-xs">Sport</a></div>
-                            <h2 class="text-lg font-semibold"><a href=""> Optimize your web desing process with rapid
-                                    prototyping </a></h2>
+                        <div class="flex item my-3 border leading-normal border-gray-300 p-3 rounded items-center">
+                            <img class="rounded-md w-20 h-20 align-middle border-none mr-2 object-cover"
+                                src="https://www.ayocirebon.com/images-cirebon/post/articles/2021/05/07/10894/masjid_pusaka_baiturrahmah_-_imy.jpg"
+                                alt="" />
+                            <div class="flex flex-col">
+                                <div class="info text-sm font-semibold mb-1"><a href="category-page.html"
+                                        class="no-underline uppercase hover:underline text-gray-700 text-xs">Sport</a></div>
+                                <h2 class="text-sm font-medium line-clamp-2"><a href=""> Optimize your web desing process
+                                        with rapid
+                                        prototyping </a></h2>
+                            </div>
                         </div>
 
-                        <div class="item my-3 border leading-normal border-gray-300 p-3 rounded">
-                            <div class="info text-sm font-semibold mb-1"><a href="category-page.html"
-                                    class="no-underline uppercase hover:underline text-gray-700 text-xs">Culture</a></div>
-                            <h2 class="text-lg font-semibold"><a href=""> Optimize your web desing process with rapid
-                                    prototyping </a></h2>
+                        <div class="flex item my-3 border leading-normal border-gray-300 p-3 rounded items-center">
+                            <img class="rounded-md w-20 h-20 align-middle border-none mr-2 object-cover"
+                                src="https://www.ayocirebon.com/images-cirebon/post/articles/2021/05/07/10894/masjid_pusaka_baiturrahmah_-_imy.jpg"
+                                alt="" />
+                            <div class="flex flex-col">
+                                <div class="info text-sm font-semibold mb-1"><a href="category-page.html"
+                                        class="no-underline uppercase hover:underline text-gray-700 text-xs">Sport</a></div>
+                                <h2 class="text-sm font-medium line-clamp-2"><a href=""> Optimize your web desing process
+                                        with rapid
+                                        prototyping </a></h2>
+                            </div>
                         </div>
 
-                        <div class="item my-3 border leading-normal border-gray-300 p-3 rounded">
-                            <div class="info text-sm font-semibold mb-1"><a href="category-page.html"
-                                    class="no-underline uppercase hover:underline text-gray-700 text-xs">SEO</a></div>
-                            <h2 class="text-lg font-semibold"><a href=""> Optimize your web desing process with rapid
-                                    prototyping </a></h2>
+                        <div class="flex item my-3 border leading-normal border-gray-300 p-3 rounded items-center">
+                            <img class="rounded-md w-20 h-20 align-middle border-none mr-2 object-cover"
+                                src="https://www.ayocirebon.com/images-cirebon/post/articles/2021/05/07/10894/masjid_pusaka_baiturrahmah_-_imy.jpg"
+                                alt="" />
+                            <div class="flex flex-col">
+                                <div class="info text-sm font-semibold mb-1"><a href="category-page.html"
+                                        class="no-underline uppercase hover:underline text-gray-700 text-xs">Sport</a></div>
+                                <h2 class="text-sm font-medium line-clamp-2"><a href=""> Optimize your web desing process
+                                        with rapid
+                                        prototyping </a></h2>
+                            </div>
                         </div>
 
+                        <div class="flex item my-3 border leading-normal border-gray-300 p-3 rounded items-center">
+                            <img class="rounded-md w-20 h-20 align-middle border-none mr-2 object-cover"
+                                src="https://www.ayocirebon.com/images-cirebon/post/articles/2021/05/07/10894/masjid_pusaka_baiturrahmah_-_imy.jpg"
+                                alt="" />
+                            <div class="flex flex-col">
+                                <div class="info text-sm font-semibold mb-1"><a href="category-page.html"
+                                        class="no-underline uppercase hover:underline text-gray-700 text-xs">Sport</a></div>
+                                <h2 class="text-sm font-medium line-clamp-2"><a href=""> Optimize your web desing process
+                                        with rapid
+                                        prototyping </a></h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+@section('script')
+    <script>
+        const swiper = new Swiper('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        });
+
+    </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @yield('head')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/swiper/6.7.0/swiper-bundle.min.css') }}" rel="stylesheet"> --}}
+    {{-- <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" /> --}}
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     @yield('style')
+    
+    {{-- <script src="{{ asset('js/swiper/6.7.0/swiper-bundle.min.js') }}"></script> --}}
+    {{-- <script src="https://unpkg.com/swiper/swiper-bundle.js"></script> --}}
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 </head>
 <body class="font-poppins">
     <x-navbar></x-navbar>
