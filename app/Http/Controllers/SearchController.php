94239f7618d175c530b0fc74632ece260381eff6
Rizal Muhamad H <rizalmuhamadh@gmail.com>
2021-09-02 15:28:38 +0700

    update search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,23 +5,52 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
+use Carbon\Carbon;
 use CyrildeWit\EloquentViewable\Support\Period;
+use MeiliSearch\Client;
 
 class SearchController extends Controller
 {
     public function index(Request $request)
     {
+        
+        $client = new Client('http://127.0.0.1:7700', 'wehealth.id');
+        
         $word = $request->word;
 
-        $post = new Post();
-        // $posts = Post::search($word, function($data, $word, $options){
-        //     return $data->search($word, ["filters"=>"feature_id = 1"]);
-        // })->orderBy('created_at', 'DESC')->take(20)->get();
-        $posts = Post::search($word)->orderBy('created_at', 'DESC')->take(20)->get();
-        
-        $popular = $post->publish()->orderByViews('desc', Period::pastDays(7))->take(5)->get();
-        
-        $menu = Category::where('order', '<>', 0)->orderBy('order', 'ASC')->get();
+        $posts = $client->index('post')->search($word, ['limit' => 20, 'filters' => 'status = PUBLISH', 'attributesToRetrieve' => [
+            'id',
+            'title',
+            'slug',
+            'description',
+            'feature_id',
+            'category_id',
+            'category_name',
+            'user_id',
+            'user',
+            'status',
+            'image',
+            'created_at',
+            'timestamp'
+        ]])->getRaw();
+
+        $popular =$client->index('post-popular')->search('', ['limit' => 5, 'filters' => 'period = '.Carbon::now()->format('mY'), 'attributesToRetrieve' => [
+            'id',
+            'title',
+            'slug',
+            'description',
+            'feature_id',
+            'category_id',
+            'category_name',
+            'user_id',
+            'user',
+            'status',
+            'image',
+            'created_at',
+            'timestamp'
+        ]])->getRaw();
+
+        $menu = $client->index('category')->search('', ['filters' => 'order > 0'])->getRaw();
 
         // return $posts;
 
