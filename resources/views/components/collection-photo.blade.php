857c9aadfcc1d7f994124a2d2188c45f29749078
Rizal muhamad H <rizalmuhamadh@gmail.com>
2022-01-06 14:20:02 +0700

    update collection photo

--- resources/views/components/collection-photo.blade.php
+++ resources/views/components/collection-photo.blade.php
@@ -5,26 +5,21 @@
     </div>
 
     <div class="container mx-auto">
-        <div class="flex flex-wrap md:space-x-2 space-x-0">
+        <div class="flex flex-wrap">
             @foreach ($photos as $item)
-                <div class="lg:w-1/3 md:w-1/2 w-full">
-                    <div class="flex-item border-0 border-gray-300 rounded-lg shadow-lg hover:shadow-xl transition-3 m-2 lg:ml-0">
-                        <a href="{{ route('photo', [$item['_source']['id'], Carbon\Carbon::parse($item['_source']['created_at'])->format('dmY'), $item['_source']['slug']]) }}">
-                            {{-- <img src="/storage/{{ $item['images'][0]['media']['cropped'] }}" alt="{{ $item['title'] }}"
-                                class="object-cover w-full block" style="height: 180px"> --}}
-                                <img src="{{ env('STORAGE').'/storage/'.$item['_source']['images'][0]['media']['original'] }}" alt="{{ $item['_source']['title'] }}"
-                                class="object-cover w-full block">
+                <div class="w-full md:w-1/2 px-2 pb-12">
+                    <div class="h-full bg-white rounded overflow-hidden shadow-md hover:shadow-lg relative smooth">
+                        <a href="{{ route('photo', [$item['_source']['id'], Carbon\Carbon::parse($item['_source']['created_at'])->format('dmY'), $item['_source']['slug']]) }}" class="no-underline hover:no-underline">
+                            <img src="{{ env('STORAGE') . '/storage/' . $item['_source']['images'][0]['media']['original'] }}"
+                            alt="{{ $item['_source']['title'] }}"
+                                class="h-48 w-full rounded-t object-cover shadow">
+                            <div class="p-6 h-auto md:h-48">
+                                <p class="text-gray-600 text-xs md:text-sm">{{ Carbon\Carbon::parse($item['_source']['created_at'])->format('d M Y, H:m') }}
+                                    WIB</p>
+                                <h2 class="font-bold text-xl text-gray-900 line-clamp-3">{{ $item['_source']['title'] }}</h2>
+                                
+                            </div>
                         </a>
-                        <h2 class="text-gray-800 font-semibold text-base leading-tight p-3 pb-0 line-clamp-2"> <a
-                                href="{{ route('photo', [$item['_source']['id'], Carbon\Carbon::parse($item['_source']['created_at'])->format('dmY'), $item['_source']['slug']]) }}">
-                                {{ $item['_source']['title'] }} </a></h2>
-                        <div class="w-full text-xs p-3 pt-2">
-                            {{-- <div class="text-gray-600"> <a href="author.html" class="text-black hover:underline">Neelam
-                                    Munir</a> in <a href="category-page.html" class="text-black hover:underline">
-                                    Culture
-                                </a> </div> --}}
-                            <div class="text-gray-600"> <span>{{ Carbon\Carbon::parse($item['_source']['created_at'])->format('d M Y, H:m') }} WIB</span> </div>
-                        </div>
                     </div>
                 </div>
             @endforeach
